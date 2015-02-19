2cb6e0d661abc0d741b82f97cd69e7f2561b2b78
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-02-20 00:32:29 +0100

    Added the problem of simple string comparison

---
+++ src/edu/tum/cs/gaylebook/problems/strcompress/App.java
@@ -0,0 +1,23 @@
+package edu.tum.cs.gaylebook.problems.strcompress;
+
+/*
+ * 
+ * Implement a method to perform basic string compression using 
+ * the counts of repeated characters. For example, the string 
+ * aabcccccaaa would become a2blc5a3. If the "compressed" string 
+ * would not become smaller than the original string, your method 
+ * should return the original string.
+ * 
+ * */
+public class App {
+	public static void main(String[] args) {
+		String input = "Hel lo";
+		String output = compressString(input);
+		System.out.println("Input: " + input + "\nOutput: " + output);
+	}
+
+	private static String compressString(String input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
