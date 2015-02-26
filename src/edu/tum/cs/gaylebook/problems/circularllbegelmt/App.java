006543cd627bc853b4b0a00c1aa53dd55dae37be
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-02-26 01:22:53 +0100

    Added solution to finding the starting node of a loop in circular LL

---
+++ src/edu/tum/cs/gaylebook/problems/circularllbegelmt/App.java
@@ -0,0 +1,45 @@
+package edu.tum.cs.gaylebook.problems.circularllbegelmt;
+
+import edu.tum.cs.essentials.linkedlist.Node;
+
+//Given a circular linked list, implement an algorithm which returns the node at the
+// beginning of the loop.
+
+public class App {
+	Node FindBeginning(Node head) {
+		Node slow = head;
+		Node fast = head;
+
+		/*
+		 * Find meeting point. This will be LOOP_SIZE - k steps into the linked
+		 * list.
+		 */
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) { // Collision
+				break;
+			}
+		}
+
+		/* Error check - no meeting point, and therefore no loop */
+		if (fast == null || fast.next == null) {
+			return null;
+		}
+
+		/*
+		 * Move slow to Head. Keep fast at Meeting Point. Each are k steps from
+		 * the Loop Start. If they move at the same pace, they must meet at Loop
+		 * Start.
+		 */
+
+		slow = head;
+		while (slow != fast) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+
+		/* Both now point to the start of the loop. */
+		return fast;
+	}
+}
