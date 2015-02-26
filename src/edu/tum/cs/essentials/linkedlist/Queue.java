c997c16580057d9376279377b7245a6be17c9a81
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-02-26 02:15:41 +0100

    Added Queue class to essentials

---
+++ src/edu/tum/cs/essentials/linkedlist/Queue.java
@@ -0,0 +1,26 @@
+package edu.tum.cs.essentials.linkedlist;
+
+public class Queue {
+
+	LinkedListNode first, last;
+
+	void enqueue(Object item) {
+		if (first == null) {
+			last = new LinkedListNode(item);
+			first = last;
+		} else {
+			last.next = new LinkedListNode(item);
+			last = last.next;
+		}
+
+	}
+
+	Object dequeue() {
+		if (first != null) {
+			Object temp = first.dataObj;
+			first = first.next;
+			return temp;
+		}
+		return null;
+	}
+}
