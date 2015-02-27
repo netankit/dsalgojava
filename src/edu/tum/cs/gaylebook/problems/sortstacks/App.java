e04e03acdbe260ecb90ebc3fd506b028428c61bd
netankit <ankit.bahuguna@cs.tum.edu>
2015-02-27 18:06:41 +0100

    Added problem - Stack sorting using single stack

---
+++ src/edu/tum/cs/gaylebook/problems/sortstacks/App.java
@@ -0,0 +1,24 @@
+package edu.tum.cs.gaylebook.problems.sortstacks;
+
+import java.util.Stack;
+
+//Stack Sorting
+/*
+ * 
+ Write a program to sort a stack in ascending order (with biggest items on top).
+ You may use at most one additional stack to hold items, but you may not copy the
+ elements into any other data structure (such as an array). The stack supports the
+ following operations: push, pop, peek, and isEmpty.
+ *
+ */
+// 
+// 1 9 3 7 2 8 5 <-- TOS
+// 
+//
+
+public class App {
+
+	public static Stack<Integer> sort(Stack<Integer> s) {
+		return s;
+	}
+}
