085cec656008dd643471893c607b409fd6dae308
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-02-28 23:48:45 +0100

    Added boiler plate code for level nodes at tree to linked list problem using both DFS and BFS

---
+++ src/edu/tum/cs/gaylebook/problems/binarytreetoll/App.java
@@ -0,0 +1,25 @@
+package edu.tum.cs.gaylebook.problems.binarytreetoll;
+
+/* 
+ * 
+ * Given a binary tree, design an algorithm which creates a 
+ * linked list of all the nodes at each depth (e.g., if you have
+ * a tree with depth D, you'll have D linked lists).
+ * 
+ */
+
+//Trick: Understand which level we are on. And thus we 
+// can avoid a level order traversal.
+
+/*
+ * Implementing a simple modification of post order traversal 
+ * where we pass in level +1 to the next recursive call. 
+ * 
+ * ==========
+ * 
+ * This code: Implementation below using DFS.
+ * 
+ */
+public class App {
+
+}
---
+++ src/edu/tum/cs/gaylebook/problems/binarytreetoll/Version2.java
@@ -0,0 +1,6 @@
+package edu.tum.cs.gaylebook.problems.binarytreetoll;
+
+//Implementation of the same problem using BFS.
+public class Version2 {
+
+}
