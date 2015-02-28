5a47775195fb6033a799ce8b1efb79f6a3b39180
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-02-28 20:05:17 +0100

    Added solution converting integer array to Minimal BST

--- src/edu/tum/cs/essentials/TreeNode.java
+++ src/edu/tum/cs/essentials/TreeNode.java
@@ -1,6 +1,11 @@
 package edu.tum.cs.essentials;
 
 public class TreeNode {
+	public TreeNode(int i) {
+		// TODO Auto-generated constructor stub
+		value = i;
+	}
+
 	public int value;
 	public TreeNode left;
 	public TreeNode right;
--- src/edu/tum/cs/gaylebook/problems/bstminimalheight/App.java
+++ src/edu/tum/cs/gaylebook/problems/bstminimalheight/App.java
@@ -1,5 +1,7 @@
 package edu.tum.cs.gaylebook.problems.bstminimalheight;
 
+import edu.tum.cs.essentials.TreeNode;
+
 public class App {
 
 	public static void main(String[] args) {
@@ -7,4 +9,21 @@ public class App {
 
 	}
 
+	TreeNode createMinimalBST(int[] arr, int start, int end) {
+		if (end < start) {
+			return null;
+		}
+
+		int mid = (start + end) / 2;
+
+		TreeNode n = new TreeNode(arr[mid]);
+		n.left = createMinimalBST(arr, start, mid - 1);
+		n.right = createMinimalBST(arr, mid + 1, end);
+		return n;
+
+	}
+
+	TreeNode createMinimalBST(int array[]) {
+		return createMinimalBST(array, 0, array.length - 1);
+	}
 }
