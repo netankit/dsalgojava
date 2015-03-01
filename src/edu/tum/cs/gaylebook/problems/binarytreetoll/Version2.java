980b9594b2df7dc70355f7ee2cfa8197b51f902d
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-03-01 02:48:16 +0100

    Added BFS based implementation of binary tree to LL problem

--- src/edu/tum/cs/gaylebook/problems/binarytreetoll/Version2.java
+++ src/edu/tum/cs/gaylebook/problems/binarytreetoll/Version2.java
@@ -1,6 +1,37 @@
 package edu.tum.cs.gaylebook.problems.binarytreetoll;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import edu.tum.cs.essentials.TreeNode;
+
 //Implementation of the same problem using BFS.
 public class Version2 {
+	public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(
+			TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
 
+		/* "Visit" the root */
+		LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+		if (root != null) {
+			current.add(root);
+		}
+
+		while (current.size() > 0) {
+			result.add(current); // Add previous level
+			LinkedList<TreeNode> parents = current; // Go to next level
+			current = new LinkedList<TreeNode>();
+			for (TreeNode parent : parents) {
+				/* Visit the children */
+				if (parent.left != null) {
+					current.add(parent.left);
+				}
+				if (parent.right != null) {
+					current.add(parent.right);
+				}
+			}
+		}
+
+		return result;
+	}
 }
