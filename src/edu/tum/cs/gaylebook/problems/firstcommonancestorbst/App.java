79a10c4cd2abdd87d20ea7b847767d6d8694312f
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-03-02 02:33:52 +0100

    added boilerplate code first common ancestor binary tree

---
+++ src/edu/tum/cs/gaylebook/problems/firstcommonancestorbst/App.java
@@ -0,0 +1,11 @@
+package edu.tum.cs.gaylebook.problems.firstcommonancestorbst;
+
+/*
+ * Design an algorithm and write code to find the first common ancestor of two nodes
+ in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
+ necessarily a binary search tree
+ * 
+ * */
+public class App {
+
+}
