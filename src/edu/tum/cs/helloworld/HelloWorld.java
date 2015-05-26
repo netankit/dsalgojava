2bf4d24fd511befee7caca4e48d484de01599ef9
Ankit Bahuguna <ankit.bahuguna@cs.tum.edu>
2015-05-26 22:50:17 +0200

    Minor Changes

--- src/edu/tum/cs/helloworld/HelloWorld.java
+++ src/edu/tum/cs/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.tum.cs.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!");
 	}
 
 }
